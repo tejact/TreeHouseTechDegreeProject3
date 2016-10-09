649481918fe04e0db0a33d0fcfe47951f43c3a8a
teja tummalapalli <teja tummalapalli>
2016-10-09 19:26:53 -0400

    Added comment to test's

--- src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -29,26 +29,41 @@ public class UserTest {
 
     @Test
     public void upVotingQuestionIncreasesReputation() throws Exception {
+        // Arrange: Given that teja and alice are users on board
+        // and "teja" asked a question
+
+        //Act: When alice  upvotes teja's question
         alice.upVote(question);
 
+        //Assert
         assertEquals(5,teja.getReputation());
     }
 
     @Test
     public void upVotingAnswerIncreasesReputation() throws Exception {
+        //Arrange: Given that teja and alice are users on board
+        // and "teja" asked a question
+        //alice answer's teja's question
         Answer answer = alice.answerQuestion(question, "Child class can extend parent..");
 
+        //Act: When teja upvotes alice's answer
         teja.upVote(answer);
 
+        //Assert
         assertEquals(10,alice.getReputation());
     }
 
     @Test
     public void acceptingAnswerIncreasesReputation() throws Exception {
+        //Arrange: Given that teja and alice are users on board
+        // and "teja" asked a question
+        //alice answer's teja's question
         Answer answer = alice.answerQuestion(question, "Child class can extend parent..");
 
+        //Act: teja accept's alice's answers
         teja.acceptAnswer(answer);
 
+        //Assert
         assertEquals(15,alice.getReputation());
     }
 
@@ -59,29 +74,42 @@ public class UserTest {
 
     @Test(expected = com.teamtreehouse.techdegree.overboard.exc.VotingException.class)
     public void upvotingOwnAnswerNotAllowed() throws Exception{
+        //Arrange: Given that teja and alice are users on board
+        // and "teja" asked a question
+        //alice answer's teja's question
         Answer answer = alice.answerQuestion(question, "Child class can extend parent..");
 
+        //Act : Alice upvote's her own answer
         alice.upVote(answer);
     }
 
 
     @Test
     public void originalQuestionerIsOnlyAllowedToAnswer() {
+        //Arrange: Given that teja and alice are users on board
+        // and "teja" asked a question
+        //alice answer's teja's question
         Answer answer = alice.answerQuestion(question, "Child class can extend parent..");
-
+        //Expect the exception
         throwns.expect(com.teamtreehouse.techdegree.overboard.exc.AnswerAcceptanceException.class);
         throwns.expectMessage("Only "+question.getAuthor().getName()+" can accept this answer as it is their question");
 
+        //Act: Alice accepts her own asnwer
         alice.acceptAnswer(answer);
     }
 
     @Test
     public void downvotingAnswerReducesReputation() {
+        //Arrange: Given that teja and alice are users on board
+        // and "teja" asked a question
+        //alice answer's teja's question
         Answer answer = alice.answerQuestion(question, "Child class can extend parent..");
 
+        //Act : Teja accepts the answer and downvotes it.
         teja.acceptAnswer(answer);
         teja.downVote(answer);
 
+        //Assert
         assertEquals(14,alice.getReputation());
     }
 
